caa8fbfd01cd5dbcc4cbf0ce1147aabfc5d34817
ayeeesaket <saketvermawid@gmail.com>
2024-11-08 23:55:01 +0530

    almost done maybe

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,32 @@
 import Navbar from "./Component/Navbar";
 import Book from "./Component/Book";
+import Slider from "./Component/Slider";
+import ActsandRules from "./Component/ActsandRules";
+import Video1 from "./Component/Video1";
+import Video2 from "./Component/Video2";
+import Video3 from "./Component/Video3";
+import Quiz from "./Component/Quiz";
+import Body from "./Component/Body";
+import Login from "./Component/Login";
+import RegisterForm from "./Component/Register";
+import Browse from "./Component/Browse";
+// import { AuthProvider } from "./context/AuthContext";
+
 const App = () => {
   return (
     <>
       {/* <Navbar /> */}
-      <Book />
+      {/* <Book /> */}
+      {/* <Slider />
+      <ActsandRules /> */}
+      {/* <Video1/> */}
+      {/* <Video2/> */}
+      {/* <Video3/> */}
+      {/* <Quiz/> */}
+      <Body/>
+      {/* <Login/> */}
+      {/* <RegisterForm/> */}
+      
     </>
   );
 };
