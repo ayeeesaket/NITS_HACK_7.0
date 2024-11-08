559d067adca7dc3e5463533c2301bc362e0dc51f
anmol420 <anmolanand2k15@gmail.com>
2024-11-08 14:33:46 +0530

    Added Minor Fixes & Changes

---
+++ backend/README.md
@@ -0,0 +1,5 @@
+```env
+PORT=8000
+CORS_ORIGIN=*
+MONGODB_URI=mongodb://127.0.0.1:27017
+```
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,22 +1,22 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
     path: './.env',
 });
 
 connectDB()
-.then(()=>{
-    app.on("error",(error)=>{ //listening for errors
-        console.log("ERR:",error);
-        throw error
-    })
+    .then(() => {
+        app.on("error", (error) => { //listening for errors
+            console.log("ERR:", error);
+            throw error
+        })
 
-    app.listen(process.env.PORT || 8000,()=>{ //starting the express server
-        console.log(`Server is running at PORT ${process.env.PORT}`);
+        app.listen(process.env.PORT || 8000, () => { //starting the express server
+            console.log(`Server is running at PORT ${process.env.PORT}`);
+        })
     })
-})
-.catch((err)=>{
-    console.log("MongoDB connection Failed!!!",err);
-    
-})
+    .catch((err) => {
+        console.log("MongoDB connection Failed!!!", err);
+    })
